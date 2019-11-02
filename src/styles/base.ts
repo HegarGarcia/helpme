e0e21e1f8a4c54d27e4632b4d10ef0798f503766
Hegar Garcia <hegargarcia@gmail.com>
2019-11-02 05:45:58 -0600

    Separating global styles

---
+++ src/styles/base.ts
@@ -0,0 +1,28 @@
+export const Colors = {
+  primary: "#2196f3",
+  secondary: "#1de9b6",
+  secondaryLigth: "#6dffe7",
+  secondaryDark: "#00b585",
+  tertiary: "",
+  white: "#FFF",
+  black: "#000"
+};
+
+export const Spacing = {
+  sm: 8,
+  md: 16,
+  lg: 24,
+  xg: 32
+};
+
+export const FontSize = {
+  sm: 8,
+  md: 16,
+  lg: 24,
+  xg: 32
+};
+
+export const Fonts = {
+  primary: "",
+  secondary: ""
+};

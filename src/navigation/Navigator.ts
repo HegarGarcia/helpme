c7c6abd95d85048c362cadf74b14b6dac1b9b057
Hegar Garcia <hegargarcia@gmail.com>
2019-11-02 05:46:27 -0600

    Improving navigator

--- src/navigation/Navigator.ts
+++ src/navigation/Navigator.ts
@@ -1,3 +1,24 @@
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 
-export default createAppContainer(createSwitchNavigator({}));
+import SignInScreen from "../screens/authentication/SignIn";
+import SignUpScreen from "../screens/authentication/SignUp";
+
+const AuthStack = createStackNavigator(
+  {
+    SignIn: SignInScreen,
+    SignUp: SignUpScreen
+  },
+  { headerMode: "none" }
+);
+
+export default createAppContainer(
+  createSwitchNavigator(
+    {
+      Auth: AuthStack
+    },
+    {
+      initialRouteName: "Auth"
+    }
+  )
+);

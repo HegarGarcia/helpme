5ab54ad7ed82e88655b8e95c06f31d4aa2eb7dd1
theoliver264 <31642690+theoliver264@users.noreply.github.com>
2019-11-02 00:12:08 -0600

    Login design
    diseño de pantalla de login

--- App.tsx
+++ App.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+//delete later
+import { Image, Button, TextInput, TouchableOpacity } from 'react-native';
+
 export default function App() {
+  function LoginHandler() {
+    console.log("Handle login events and sht")
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <View></View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });

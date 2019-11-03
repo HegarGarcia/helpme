6bb208e4b7e369c206e947c5efc7215a5dc19aa9
theoliver264 <31642690+theoliver264@users.noreply.github.com>
2019-11-03 09:01:35 -0600

    MarkerDialog changes

--- src/components/AddMarkerDialog.tsx
+++ src/components/AddMarkerDialog.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
+import { Picker, StyleSheet, View } from "react-native";
 import {
   Dialog,
   Portal,
@@ -6,12 +7,15 @@ import {
   Button,
   TextInput
 } from "react-native-paper";
+import { Spacing, Colors, FontSize } from "../styles/base";
 
 interface AddMarkerDialogProps {
   toggleVisibility: () => void;
 }
 
 const AddMarkerDialog: FC<AddMarkerDialogProps> = ({ toggleVisibility }) => {
+  const [selectedPickerValue, setSelectedPickerValue] = useState("");
+
   return (
     <Portal>
       <Dialog visible onDismiss={toggleVisibility}>
@@ -19,10 +23,30 @@ const AddMarkerDialog: FC<AddMarkerDialogProps> = ({ toggleVisibility }) => {
         <Dialog.Content>
           <Paragraph>¿Quien lo necesita?</Paragraph>
           <TextInput
-            placeholder={"Nombre o forma de identificarlo "}></TextInput>
+            placeholder={"Nombre o forma de identificarlo "}
+          ></TextInput>
 
           <Paragraph>¿Que se necesita?</Paragraph>
-          <TextInput placeholder={"Ej: Zapatos, Ropa, Agua"}></TextInput>
+          <View style={styles.container}>
+            <Picker
+              style={styles.picker}
+              selectedValue={selectedPickerValue}
+              onValueChange={itemValue => {
+                setSelectedPickerValue(itemValue);
+                console.log(itemValue);
+              }}
+            >
+              <Picker.Item label="Comida" value="Comida" />
+              <Picker.Item label="Agua" value="Agua" />
+              <Picker.Item label="Atencion Medica" value="Atencion Medica" />
+              <Picker.Item label="Ropa" value="Ropa" />
+              <Picker.Item label="Zapatos" value="Zapatos" />
+              <Picker.Item label="Dinero" value="Dinero" />
+            </Picker>
+          </View>
+
+          <Paragraph>Detalles</Paragraph>
+          <TextInput></TextInput>
 
           <Paragraph>Referencia de ubicación</Paragraph>
           <TextInput placeholder={"Un lugar identificable"}></TextInput>
@@ -31,8 +55,9 @@ const AddMarkerDialog: FC<AddMarkerDialogProps> = ({ toggleVisibility }) => {
           style={{
             flexDirection: "row",
             justifyContent: "space-between"
-          }}>
-          <Button color='#F00' onPress={toggleVisibility}>
+          }}
+        >
+          <Button color="#F00" onPress={toggleVisibility}>
             Cerrar
           </Button>
           <Button onPress={() => {}}>Publicar</Button>
@@ -41,5 +66,20 @@ const AddMarkerDialog: FC<AddMarkerDialogProps> = ({ toggleVisibility }) => {
     </Portal>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: Spacing.sm,
+    backgroundColor: "rgba(0, 0, 0, 0.08)",
+    borderRadius: 5,
+    marginVertical: Spacing.sm
+  },
+  picker: {
+    textAlign: "center",
+    padding: Spacing.sm,
+    fontWeight: "500",
+    fontSize: FontSize.md
+  }
+});
 
 export default AddMarkerDialog;
